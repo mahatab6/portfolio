bd04edaa13cc50ea25f1da0b9edc1be23c5006fc
mahatab6 <mahatabcall56@gmail.com>
2025-06-28 23:41:06 +0600

    theme color select

--- src/components/Main Components/Navbar.jsx
+++ src/components/Main Components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
 
     return (
         
-        <div className='bg-base-100 shadow-sm '>
+        <div className='bg-black shadow-sm '>
             <div className="navbar w-9/12 mx-auto">
                 <div className="navbar-start">
                     <div className="dropdown">
