6a61d2c4e661e662568ffc79bf0d63cfc90882d2
mahatab6 <mahatabcall56@gmail.com>
2025-06-29 20:09:14 +0600

    Logo link added

--- src/components/Reusable Components/Logo.jsx
+++ src/components/Reusable Components/Logo.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import code from '../../assets/logo.png'
+import { Link } from 'react-router';
 
 const Logo = () => {
     return (
-        <div className='flex items-center '>
+        <Link to="/" className='flex items-center '>
             <img className='w-[35px]' src={code} alt="" />
             <p className='text-xl font-bold pl-1 text-[#00B477]'>mahatab</p>
-        </div>
+        </Link>
     );
 };
 
