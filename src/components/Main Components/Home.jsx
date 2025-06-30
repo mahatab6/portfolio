4d4238ec83bafe54e5344ad1c6b60a37804d6eda
mahatab6 <mahatabcall56@gmail.com>
2025-06-30 09:33:19 +0600

    page title added

--- src/components/Main Components/Home.jsx
+++ src/components/Main Components/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Hero from '../../pages/Hero';
 import AboutMe from '../../pages/AboutMe';
+import { Helmet } from 'react-helmet';
 
 const Home = () => {
     return (
         <div>
+            <Helmet>
+                <title>Mahatab Portfolio</title>
+            </Helmet>
             <Hero/>
             <AboutMe />
         </div>
