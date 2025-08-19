24917dc986e3156bffeecdf0062c727484878aa2
mahatab6 <mahatabcall56@gmail.com>
2025-08-19 22:01:16 +0600

    about section update

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -7,7 +7,7 @@ const AboutMe = () => {
   return (
     <section id="about">
       <div className='bg-[#060407] pt-20 min-h-screen'>
-      <div className='w-11/12 md:w-9/12 mx-auto border border-green-500 p-10'>
+      <div className='w-11/12 md:w-9/12 mx-auto  p-10'>
         <h2 className='text-3xl text-center text-green-400 font-bold pb-5'>About Me</h2>
 
         <div className='md:flex mb-10'>
@@ -16,32 +16,24 @@ const AboutMe = () => {
           </div>
           <div className='flex-1 text-white'>
             <p className='text-xl p-2'>
-              Hello, I'm <span className='text-4xl text-green-400 font-bold'>Mahatab Hosen Raju</span>
+              Hi! I'm <span className='font-bold'>Md. Mahatab Hosen Raju</span>, a passionate <span className='font-bold'>MERN Stack Developer</span> who loves building clean, responsive, and user-friendly web applications.
             </p>
             <p className='text-xl p-2 max-w-3xl'>
-              I'm a passionate and proactive MERN Stack Developer from Bangladesh. I specialize in building dynamic, scalable, and user-friendly web applications using React, Next.js, Node.js, Express, and MongoDB.
+              My programming journey started with curiosity about how websites work, and over time I mastered <span className='font-bold'>JavaScript, React, Node.js, Express, and MongoDB.</span>  I enjoy developing projects that solve real-world problems — from<span className='font-bold'> authentication systems to role-based dashboards and seamless UI/UX experiences.</span>
             </p>
             <p className='text-xl p-2 max-w-3xl'>
-              With a strong focus on clean code, modern UI/UX, and performance optimization, I love transforming ideas into real-world applications. Whether it's crafting intuitive front-end experiences or architecting robust back-end systems, I’m always exploring new tools, frameworks, and technologies to level up my development skills.
+            I believe in continuous learning and my career objective is to grow as a professional developer while exploring fields like <span className='font-bold'>Artificial Intelligence, Cloud Deployment, and Sustainable Tech Solutions.</span>
             </p>
             <p className='text-xl p-2 max-w-3xl pb-2'>
-              🌱 Constantly learning, always building. Let's create something amazing, one project at a time.
+              Beyond coding, I enjoy traveling, keeping up with tech trends, and sometimes playing cricket. 🚀
             </p>
 
             <div className="divider divider-success"></div>
               <div>
                 <p className='text-4xl font-bold text-gray-200 mb-8'>Education</p>
                 <div>
-                  <h2 className='text-xl font-bold text-green-500 flex items-center gap-1'><GoDotFill /> Secondary School</h2>
+                  <h2 className='text-xl font-bold text-green-500 flex items-center gap-1'><GoDotFill /> Diploma in Computer Science and Technology (2022 – Present)</h2>
                   <div className='px-5'>
-                    <p className='text-base font-semibold '>2019 - 2021</p>
-                    <p className='text-base font-semibold '>Konabari M A Kuddus High School</p>
-                  </div>
-                </div>
-                <div>
-                  <h2 className='text-xl font-bold text-green-500 flex items-center gap-1'><GoDotFill /> Diploma in CST</h2>
-                  <div className='px-5'>
-                    <p className='text-base font-semibold '>2021 - 2025</p>
                     <p className='text-base font-semibold '>Barishal Polytechnic Institute</p>
                   </div>
                 </div>
